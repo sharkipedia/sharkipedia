b7ae8d59b76bc0a5db687943e53d259a5749ea03
Maximilian Haack <mxhaack@gmail.com>
2019-02-04 19:44:10 -0500

    Add empty HomeController

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#start'
 
+  get 'home/index'
+
   # static pages
   match '/about',   to: 'pages#about',   via: 'get'
 end

39b9cd1ca57044c337c00620ed57209935a6aebc
Maximilian Haack <mxhaack@gmail.com>
2019-02-08 11:52:33 -0500

    Fix `belongs_to` in `Observation`

--- app/models/observation.rb
+++ app/models/observation.rb
@@ -1,7 +1,7 @@
 class Observation < ApplicationRecord
   belongs_to :user
   belongs_to :resource
-  belongs_to :secondary_resource, class: 'Resource'
+  belongs_to :secondary_resource, class_name: 'Resource'
   belongs_to :species
   belongs_to :longhurst_province
   belongs_to :location

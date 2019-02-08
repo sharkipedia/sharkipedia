7ea7452000ab6a8353dcfd1ca07be0a1d9b5bdaa
Maximilian Haack <mxhaack@gmail.com>
2019-02-08 16:43:51 -0500

    Limit access to admin dashboards to admin/editor

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -6,10 +6,13 @@
 # you're free to overwrite the RESTful controller actions.
 module Admin
   class ApplicationController < Administrate::ApplicationController
+    before_action :authenticate_user!
     before_action :authenticate_admin
 
     def authenticate_admin
-      # TODO Add authentication logic here.
+      unless current_user.admin? || current_user.editor?
+        redirect_to root_path
+      end
     end
 
     # Override this value to specify the number of elements to display at a time

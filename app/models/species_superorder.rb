97220895feeba377124849f9e6840171e88ca8ad
Maximilian Haack <max@haack.xyz>
2019-03-31 21:15:58 +0800

    Add missing Superorder -> Subclass belongs_to

--- app/models/species_superorder.rb
+++ app/models/species_superorder.rb
@@ -1,6 +1,8 @@
 class SpeciesSuperorder < ApplicationRecord
   validates :name, presence: true, uniqueness: true
 
+  belongs_to :species_subclass
+
   has_many :species
   has_many :species_orders
   has_many :species_families

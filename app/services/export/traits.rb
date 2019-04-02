d244f949ed8d40521509eaa2d4037ca7b55ce5f4
Maximilian Haack <max@haack.xyz>
2019-04-02 19:17:57 -0400

    Add Traits to CSV exporter

---
+++ app/services/export/traits.rb
@@ -0,0 +1,49 @@
+module Export
+  class Traits < ApplicationService
+    def initialize(observations=nil)
+      @observations = observations || Observation.published
+    end
+
+    def call
+      CSV.generate do |csv|
+        csv << %w{
+resource_name resource_doi species_superorder species_name marine_province location_name lat long date sex trait_class trait_name standard_name method_name model_name value value_type precision precision_type precision_upper sample_size dubious validated validation_type notes contributor_id depth 
+        }
+
+        @observations.each do |observation|
+          observation.measurements.each do |measurement|
+            csv << [
+              observation.resources.first.name,
+              observation.resources.first.doi,
+              observation.species.species_superorder.name,
+              observation.species.name,
+              observation.longhurst_province.try(:name),
+              observation.location.name,
+              observation.location.lat,
+              observation.location.lon,
+              observation.date,
+              measurement.sex_type.name,
+              measurement.trait_class.name,
+              measurement.trait.name,
+              measurement.standard.try(:name),
+              measurement.measurement_method.try(:name),
+              measurement.measurement_model.try(:name),
+              measurement.value,
+              measurement.value_type.try(:name),
+              measurement.precision,
+              measurement.precision_type.try(:name),
+              measurement.precision_upper,
+              measurement.sample_size,
+              measurement.dubious,
+              measurement.validated,
+              measurement.validation_type.try(:name),
+              measurement.notes,
+              observation.contributor_id,
+              observation.depth,
+            ]
+          end
+        end
+      end
+    end
+  end
+end

23b5d818294c099216112ce2f7268cb696e9d08a
Maximilian Haack <max@haack.xyz>
2019-04-03 08:46:28 -0400

    Add Trends CSV exporter

---
+++ app/services/export/trends.rb
@@ -0,0 +1,70 @@
+module Export
+  class Trends < ApplicationService
+    def initialize(trends=nil)
+      @trends = trends || Trend.all
+    end
+
+    def call
+      CSV.generate do |csv|
+        header = %w{
+          Class Order Family Genus Species Binomial IUCNcode SourceYear
+          Taxonomic Notes AuthorYear DataSource doi
+        }
+
+        years = TrendObservation.pluck(:year).uniq.sort
+        header += years
+
+        header += %w{
+          Units SamplingMethod SamplingMethodCode Location Latitude Longitude
+          Ocean DataType NoYears TimeMin PageAndFigureNumber LineUsed PDFPage
+          ActualPage Depth Model FigureName FigureData
+        }
+
+        csv << header
+
+        @trends.each do |trend|
+          trend.trend_observations.each do |observation|
+            data = [
+              trend.species.species_subclass.name,
+              trend.species.species_superorder.name,
+              trend.species.species_family.name,
+              nil,
+              trend.species.name,
+              trend.species.iucn_code,
+              trend.resource.year,
+              trend.taxonomic_notes,
+              trend.resource.name,
+              trend.resource.data_source,
+              trend.resource.doi,
+            ]
+
+            data += trend.trend_observations.pluck(:year, :value).sort.to_h
+                         .values_at(*years)
+
+            data += [
+              trend.standard.name,
+              trend.sampling_method.name,
+              trend.sampling_method.code,
+              trend.location.name,
+              trend.location.lat,
+              trend.location.lon,
+              trend.ocean.name,
+              trend.data_type.name,
+              trend.no_years,
+              trend.time_min,
+              trend.page_and_figure_number,
+              trend.line_used,
+              trend.pdf_page,
+              trend.actual_page,
+              trend.depth,
+              trend.model,
+              trend.figure_name,
+              trend.figure_data,
+            ]
+            csv << data
+          end
+        end
+      end
+    end
+  end
+end

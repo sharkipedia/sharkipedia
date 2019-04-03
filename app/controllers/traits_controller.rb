52b4afd452381277d3577a6b457150a39161c36a
Maximilian Haack <max@haack.xyz>
2019-04-02 20:47:37 -0400

    Add Traits CSV download link

--- app/controllers/traits_controller.rb
+++ app/controllers/traits_controller.rb
@@ -2,5 +2,10 @@ class TraitsController < PreAuthController
   def index
     observations = current_user.try(:editor?) ? Observation.all : nil
     @traits = Export::Traits.new(observations).call
+
+    respond_to do |format|
+      format.html
+      format.csv { send_data @traits, filename: "traits-#{Date.today}.csv" }
+    end
   end
 end

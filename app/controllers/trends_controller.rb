2e01d034041fe5269ba98d9a04bbfeae1e3caa72
Maximilian Haack <max@haack.xyz>
2019-04-23 17:46:46 -0400

    Use Xlsx.js to generate the downloadable CSV
    No need to hit the backend again as the client already has all the data

--- app/controllers/trends_controller.rb
+++ app/controllers/trends_controller.rb
@@ -1,10 +1,5 @@
 class TrendsController < ApplicationController
   def index
     @trends = Export::Trends.new.call
-
-    respond_to do |format|
-      format.html
-      format.csv { send_data @trends, filename: "trends-#{Date.today}.csv" }
-    end
   end
 end

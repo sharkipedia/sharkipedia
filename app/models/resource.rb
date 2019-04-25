7b1b67fc95d80698ece1082d1ccd52d7b401ed69
Maximilian Haack <max@haack.xyz>
2019-04-24 20:31:07 -0400

    Add DOI validation for resources

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -3,4 +3,8 @@ class Resource < ApplicationRecord
 
   has_and_belongs_to_many :observations
   has_many :trends
+
+  validates :doi, format: {
+    with: /\A10.\d{4,9}\/[-._;()\/:A-Z0-9]+\z/i,
+    message: 'malformed, please follow specification.' }, allow_blank: true
 end
--- spec/models/resource_spec.rb
+++ spec/models/resource_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Resource, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  [
+    [true, '10.1111/jfb.12087'],
+    [true, nil],
+    [true, ''],
+    [false, '0.1111/jfb.12087'],
+  ].each do |valid, doi|
+    it "DOI #{doi.inspect} valid? to be #{valid}" do
+      resource = Resource.new name: 'some resource', doi: doi
+      expect(resource.valid?).to be(valid)
+    end
+  end
 end

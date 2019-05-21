fb4ecec6198ac36669ce1345443feb16ecae1ea3
Maximilian Haack <max@haack.xyz>
2019-05-20 21:18:20 -0500

    Fix typo in email configuration for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,7 +90,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.smtp_settings = {
-    :address => ENV["SES_SMTP_USERNAME"] || Rails.application.credentials.dig(:gmail, :smtp_host),
+    :address => ENV["SES_SMTP_HOST"] || Rails.application.credentials.dig(:gmail, :smtp_host),
     :port => 587,
     :user_name => ENV["SES_SMTP_USERNAME"] || Rails.application.credentials.dig(:gmail, :smtp_username),
     :password => ENV["SES_SMTP_PASSWORD"] || Rails.application.credentials.dig(:gmail, :smtp_password),

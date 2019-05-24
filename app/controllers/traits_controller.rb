69f0ee910b90672860f00ef7ce9923d9551bb338
Sebastian Pardo <sebpardo@gmail.com>
2019-05-24 13:47:53 -0300

    Create landing page with basic info and current traits data counts

--- app/controllers/traits_controller.rb
+++ app/controllers/traits_controller.rb
@@ -1,5 +1,8 @@
 class TraitsController < PreAuthController
   def index
+    @measurement_count = Measurement.count
+    @trait_count       = Measurement.joins(:trait).select(:trait_id).distinct.count #Trait.count
+    @species_count     = Observation.joins(:species).select(:species_id).distinct.count
     observations = current_user.try(:editor?) ? Observation.all : nil
     @traits = Export::Traits.new(observations).call
   end

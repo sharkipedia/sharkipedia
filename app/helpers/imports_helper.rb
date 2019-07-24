cea7c83fc08c8b64b946f81f54e790f7b43ed144
Maximilian Haack <max@haack.xyz>
2019-07-24 19:27:38 +0200

    Link to uploader in admin dashboard
    If the current user is an admin, link to the user in the admin dashboard
    from the import overview and detail pages.

--- app/helpers/imports_helper.rb
+++ app/helpers/imports_helper.rb
@@ -1,2 +1,9 @@
 module ImportsHelper
+  def link_to_admin_dashboard import
+    if current_user.admin?
+      link_to import.uploaded_by, admin_user_path(import.user)
+    else
+      import.uploaded_by
+    end
+  end
 end

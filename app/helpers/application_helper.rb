1686455d64cb5139bf47dc38bb5de5b1e60c3d45
Maximilian Haack <max@haack.xyz>
2019-07-25 16:13:12 +0200

    Add link_to_doi helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def user_is_admin?
     current_user.try(:admin?)
   end
+
+  def link_to_doi doi
+    link_to doi, "https://www.doi.org/#{doi}", target: '_blank'
+  end
 end

e10e448bf2bd60c1c291de97eb11a2b80827acc0
Maximilian Haack <max@haack.xyz>
2019-07-25 16:32:39 +0200

    Add mocked Trend form

--- app/controllers/trends_controller.rb
+++ app/controllers/trends_controller.rb
@@ -4,4 +4,44 @@ class TrendsController < ApplicationController
   def index
     @trends = Export::Trends.new.call
   end
+
+  def new
+    @example_specie = Species.find_by name: 'Carcharhinus acronotus'
+    @example_resource = Resource.find_by name: 'everett2015'
+    @trend = current_user.trends.new
+  end
+
+  def show
+    @trend = Trend.find params[:id]
+  end
+
+  def create
+    @trend = current_user.trends.new(trend_params)
+
+    if @trend.save
+      redirect_to @trend
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def trend_params
+    params.require(:trend).permit(
+      :no_years,
+      :time_min,
+      :taxonomic_notes,
+      :page_and_figure_number,
+      :line_used,
+      :pdf_page,
+      :actual_page,
+      :depth,
+      :model,
+      :figure_name,
+      :figure_data,
+      trend_observations_attributes: [ :id, :url, :alt, :caption ],
+      variants_attributes: [ :id, :name, :price, :_destroy ]
+    )
+  end
 end
--- app/models/trend.rb
+++ app/models/trend.rb
@@ -8,5 +8,7 @@ class Trend < ApplicationRecord
   belongs_to :standard
   belongs_to :sampling_method
 
-  has_many :trend_observations
+  has_many :trend_observations, dependent: :destroy
+
+  accepts_nested_attributes_for :trend_observations
 end

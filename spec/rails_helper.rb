4dc9ee0c386ad0cde27e84c0128559064c6cf20c
Maximilian Haack <max@haack.xyz>
2019-08-27 12:53:38 -0400

    Remove fixture_path config from spec/rails_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -33,9 +33,6 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 RSpec.configure do |config|
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
   config.include FactoryBot::Syntax::Methods
 
   config.include Devise::Test::ControllerHelpers, :type => :controller

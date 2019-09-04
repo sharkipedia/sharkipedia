4e4b0bf0efaec3099d4ae55a9ed76d5285ef9e7c
Maximilian Haack <max@haack.xyz>
2019-09-04 14:49:59 -0400

    Show trend data on Species detail page

--- app/assets/javascripts/trends.js
+++ app/assets/javascripts/trends.js
@@ -1,4 +1,76 @@
 $(document).on('turbolinks:load', function() {
+  function renderTrendChart() {
+    var trendData = document.querySelectorAll('#trend-data td')
+    if (trendData === null) {
+      return;
+    }
+
+    var years = [];
+    var values = [];
+    for (i = 0; i < trendData.length; i++) {
+      if (i % 2 === 0) {
+        years.push(new Date(trendData[i].textContent));
+      } else {
+        let val = trendData[i].textContent;
+        values.push(val === "" ? null : val);
+      }
+    }
+
+    var ctx = document.getElementById('trend-chart').getContext('2d');
+
+    // uncomment this to interpolate between missing data points
+    // Chart.defaults.line.spanGaps = true;
+
+    const data = {
+      labels: years,
+      datasets: [{
+        fill: false,
+        data: values,
+        borderColor: '#fe8b36',
+        backgroundColor: '#fe8b36',
+        lineTension: 0,
+      }]
+    }
+    const options = {
+      type: 'line',
+      data: data,
+      options: {
+        fill: false,
+        responsive: true,
+        tooltips: {
+          enabled: false,
+        },
+        legend: {
+          display: false
+        },
+        scales: {
+          xAxes: [{
+            type: 'time',
+            display: true,
+            scaleLabel: {
+              display: true,
+            },
+            distribution: 'series',
+            time: {
+              unit: 'year'
+            }
+          }],
+          yAxes: [{
+            ticks: {
+              beginAtZero: true,
+            },
+            display: true,
+            scaleLabel: {
+              display: true,
+            }
+          }]
+        }
+      }
+    }
+    const chart = new Chart(ctx, options);
+  }
+  renderTrendChart();
+
   if (!checkController('trends')) {
     return;
   }
@@ -201,74 +273,5 @@ $(document).on('turbolinks:load', function() {
     document.getElementById('trend_start_year')
       .addEventListener('change', calcNumYears);
   } else if (checkController('show')) {
-    function renderChart() {
-      var trendData = document.querySelectorAll('#trend-data td')
-
-      var years = [];
-      var values = [];
-      for (i = 0; i < trendData.length; i++) {
-        if (i % 2 === 0) {
-          years.push(new Date(trendData[i].textContent));
-        } else {
-          let val = trendData[i].textContent;
-          values.push(val === "" ? null : val);
-        }
-      }
-
-      var ctx = document.getElementById('trend-chart').getContext('2d');
-
-      // uncomment this to interpolate between missing data points
-      // Chart.defaults.line.spanGaps = true;
-
-      const data = {
-        labels: years,
-        datasets: [{
-          fill: false,
-          data: values,
-          borderColor: '#fe8b36',
-          backgroundColor: '#fe8b36',
-          lineTension: 0,
-        }]
-      }
-      const options = {
-        type: 'line',
-        data: data,
-        options: {
-          fill: false,
-          responsive: true,
-          tooltips: {
-            enabled: false,
-          },
-          legend: {
-            display: false
-          },
-          scales: {
-            xAxes: [{
-              type: 'time',
-              display: true,
-              scaleLabel: {
-                display: true,
-              },
-              distribution: 'series',
-              time: {
-                unit: 'year'
-              }
-            }],
-            yAxes: [{
-              ticks: {
-                beginAtZero: true,
-              },
-              display: true,
-              scaleLabel: {
-                display: true,
-              }
-            }]
-          }
-        }
-      }
-      const chart = new Chart(ctx, options);
-    }
-
-    renderChart();
   }
 });

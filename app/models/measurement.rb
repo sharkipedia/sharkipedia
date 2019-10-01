646ae918bdd0c31fa74d4d7b0177622efd3e44e0
Maximilian Haack <max@haack.xyz>
2019-10-01 15:14:45 -0400

    Find or create a measurement's location
    Doing this through a callback since we're creating the locaions through
    the new trait form.

--- app/models/measurement.rb
+++ app/models/measurement.rb
@@ -1,6 +1,5 @@
 class Measurement < ApplicationRecord
   belongs_to :observation
-  belongs_to :location
   belongs_to :longhurst_province, optional: true
   belongs_to :sex_type
   belongs_to :trait_class
@@ -11,4 +10,15 @@ class Measurement < ApplicationRecord
   belongs_to :value_type, optional: true
   belongs_to :validation_type, optional: true
   belongs_to :precision_type, optional: true
+
+  belongs_to :location
+  accepts_nested_attributes_for :location, allow_destroy: true
+
+  before_save :find_or_create_location
+
+  private
+
+  def find_or_create_location
+    self.location = Location.find_or_create_by name: location.name
+  end
 end

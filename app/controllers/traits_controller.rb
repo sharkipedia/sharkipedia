010ac01fc2908915056226dc2c052b987fdbf411
Maximilian Haack <max@haack.xyz>
2019-11-04 11:12:51 -0500

    Fix n+1 queries in TraitsController

--- app/controllers/traits_controller.rb
+++ app/controllers/traits_controller.rb
@@ -2,15 +2,17 @@ class TraitsController < PreAuthController
   include Pagy::Backend
 
   def index
-    # TODO: cache these calculations
-    @measurement_count = Measurement.count
-    @trait_count       = Measurement.joins(:trait).select(:trait_id).distinct.count
-    @species_count     = Observation.joins(:species).select(:species_id).distinct.count
-
-    @trait_classes = TraitClass.all
+    @trait_classes = TraitClass.includes(:traits).all
   end
 
   def show
-    @trait = Trait.find params[:id]
+    @trait = Trait.includes(measurements: [
+      :standard,
+      :value_type,
+      :location,
+      observation: [
+        :species,
+      ],
+    ]).find params[:id]
   end
 end

27132a8efa1195dccc4b7e8a326a2ffbe3036bc9
Maximilian Haack <max@haack.xyz>
2019-11-16 19:01:18 -0500

    Set puma concurrency to 1 in development

--- config/puma.rb
+++ config/puma.rb
@@ -20,7 +20,7 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 # the concurrency of the application would be max `threads` * `workers`.
 # Workers do not work on JRuby or Windows (both of which do not support
 # processes).
-workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+workers ENV.fetch("WEB_CONCURRENCY") { ENV["RAILS_ENV"] != "development" ? 2 : 1 }
 
 # Use the `preload_app!` method when specifying a `workers` number.
 # This directive tells Puma to first boot the application and load code

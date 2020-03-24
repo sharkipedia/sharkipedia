b24c2fe4f032be7fd0e77f254a9ec3d136a4cb14
Maximilian Haack <max@haack.xyz>
2020-03-24 21:07:38 +0100

    Add CanvasDatagrid component

---
+++ app/javascript/components/CanvasDatagrid.js
@@ -0,0 +1,44 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import PropTypes from "prop-types"
+import 'canvas-datagrid';
+
+class CanvasDatagrid extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  updateAttributes(nextProps) {
+    Object.keys(this.props).forEach(key => {
+      if (!nextProps || this.props[key] !== nextProps[key]) {
+        if (this.grid.attributes[key] !== undefined) {
+          this.grid.attributes[key] = nextProps ? nextProps[key] : this.props[key];
+        } else {
+          this.grid[key] = nextProps ? nextProps[key] : this.props[key];
+        }
+      }
+    });
+  }
+
+  componentDidUpdate() {
+    this.updateAttributes();
+  }
+
+  componentWillUnmount() {
+    this.grid.dispose();
+  }
+
+  componentDidMount() {
+    this.grid = ReactDOM.findDOMNode(this);
+    this.updateAttributes();
+  }
+
+  render() {
+    return React.createElement('canvas-datagrid', { } );
+  }
+}
+
+CanvasDatagrid.propTypes = {
+  data: PropTypes.array
+};
+export default CanvasDatagrid

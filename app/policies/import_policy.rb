795ef769d967af284788dc3b8a72eef530e913ce
Maximilian Haack <max@haack.xyz>
2020-04-23 14:44:29 +0200

    Use ImportPolicy in ImportsController

--- app/policies/import_policy.rb
+++ app/policies/import_policy.rb
@@ -1,7 +1,51 @@
 class ImportPolicy < ApplicationPolicy
+  def show?
+    user.editor? || record.user == user
+  end
+
+  def new?
+    user.contributor?
+  end
+
+  def create?
+    user.contributor?
+  end
+
+  def edit?
+    user.admin? || (
+      record.import&.user == user && record.import&.state == 'changes requested'
+    )
+  end
+
+  def update?
+    user.admin? || (
+      record.import&.user == user && record.import&.state == 'changes requested'
+    )
+  end
+
+  def request_review?
+    user.editor? || record.user == user
+  end
+
+  def request_changes?
+    user.editor?
+  end
+
+  def reject?
+    user.editor?
+  end
+
+  def approve?
+    user.editor?
+  end
+
   class Scope < Scope
     def resolve
-      scope.all
+      if user.admin? || user.editor?
+        scope.all
+      else
+        scope.where(user: user)
+      end
     end
   end
 end

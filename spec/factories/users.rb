d60ab7d8e1a39092e40de6b051d6b2132429ce7f
Maximilian Haack <max@haack.xyz>
2020-05-21 15:43:06 +0200

    Clean up user factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,23 +5,23 @@ FactoryBot.define do
     user_level { "user" }
     password { "123456789!password" }
     confirmed_at { Time.now.utc }
-  end
 
-  factory :editor, parent: :user do
-    sequence(:name) { |n| "editor #{n}" }
-    sequence(:email) { |n| "editor#{n}@example.com" }
-    user_level { "editor" }
-  end
+    factory :editor do
+      sequence(:name) { |n| "editor #{n}" }
+      sequence(:email) { |n| "editor#{n}@example.com" }
+      user_level { "editor" }
+    end
 
-  factory :contributor, parent: :user do
-    sequence(:name) { |n| "contributor #{n}" }
-    sequence(:email) { |n| "contributor#{n}@example.com" }
-    user_level { "contributor" }
-  end
+    factory :contributor do
+      sequence(:name) { |n| "contributor #{n}" }
+      sequence(:email) { |n| "contributor#{n}@example.com" }
+      user_level { "contributor" }
+    end
 
-  factory :admin, parent: :user do
-    sequence(:name) { |n| "admin #{n}" }
-    sequence(:email) { |n| "admin#{n}@example.com" }
-    user_level { "admin" }
+    factory :admin do
+      sequence(:name) { |n| "admin #{n}" }
+      sequence(:email) { |n| "admin#{n}@example.com" }
+      user_level { "admin" }
+    end
   end
 end

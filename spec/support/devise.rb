607ddb6841112b077862641ea240b35c05860154
Maximilian Haack <max@haack.xyz>
2020-05-23 19:11:05 +0200

    Load devise helpers for feature specs

--- spec/support/devise.rb
+++ spec/support/devise.rb
@@ -19,6 +19,7 @@ end
 RSpec.configure do |config|
   config.include Devise::Test::IntegrationHelpers, type: :request
   config.include Devise::Test::IntegrationHelpers, type: :system
+  config.include Devise::Test::IntegrationHelpers, type: :feature
   config.include Devise::Test::ControllerHelpers, type: :controller
 
   config.extend ControllerMacros, type: :controller

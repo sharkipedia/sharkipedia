8522d11f3836bc43f32131399bb9c91aef0191cf
Maximilian Haack <max@haack.xyz>
2020-05-23 20:28:58 +0200

    Add Imports factories

--- spec/factories.rb
+++ spec/factories.rb
@@ -7,4 +7,24 @@ FactoryBot.define do
     sequence(:name) { |n| "standard #{n}" }
     trait_class
   end
+
+  factory :import do
+    sequence(:title) { |n| "import #{n}" }
+    aasm_state { "pending_review" }
+    xlsx_valid { true }
+    log { "" }
+    user
+
+    factory :traits_import do
+      sequence(:title) { |n| "traits import #{n}" }
+      import_type { "traits" }
+      xlsx_file { Rack::Test::UploadedFile.new("spec/fixtures/xlsx/traits_valid.xlsx") }
+    end
+
+    factory :trends_import do
+      sequence(:title) { |n| "traits import #{n}" }
+      import_type { "traits" }
+      xlsx_file { Rack::Test::UploadedFile.new("spec/fixtures/xlsx/trends_valid.xlsx") }
+    end
+  end
 end

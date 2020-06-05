d9d037640b76010350323f30312d542c5dd42e30
Maximilian Haack <max@haack.xyz>
2020-06-05 14:24:48 +0200

    Add basic association / validation specs

--- spec/models/trend_observation_spec.rb
+++ spec/models/trend_observation_spec.rb
@@ -1,5 +1,11 @@
 require "rails_helper"
 
 RSpec.describe TrendObservation, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { should belong_to(:trend) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:year) }
+  end
 end
--- spec/models/trend_spec.rb
+++ spec/models/trend_spec.rb
@@ -1,5 +1,38 @@
 require "rails_helper"
 
 RSpec.describe Trend, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { should belong_to(:user) }
+    it { should belong_to(:import) }
+    it { should belong_to(:reference) }
+    it { should belong_to(:species).optional }
+    it { should belong_to(:species_group).optional }
+    it { should belong_to(:location) }
+    it { should belong_to(:ocean) }
+    it { should belong_to(:data_type) }
+    it { should belong_to(:standard) }
+    it { should belong_to(:sampling_method) }
+
+    it { should have_many(:trend_observations) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:start_year) }
+    it { should validate_presence_of(:end_year) }
+
+    context "with Species" do
+      subject { build(:trend) }
+      it { should be_valid }
+    end
+
+    context "with SpeciesGroup" do
+      subject { build(:trend_with_species_group) }
+      it { should be_valid }
+    end
+
+    context "without Species or SpeciesGroup" do
+      subject { build(:trend, species: nil, species_group: nil) }
+      it { should_not be_valid }
+    end
+  end
 end

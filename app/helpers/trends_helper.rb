f2e5beb4237607e6519bd4a30848099cc5717e54
Maximilian Haack <max@haack.xyz>
2020-06-21 15:54:28 +0200

    Add Unit sub-types to unit info block

--- app/helpers/trends_helper.rb
+++ app/helpers/trends_helper.rb
@@ -1,2 +1,7 @@
 module TrendsHelper
+  def paragraph_if_present name, attr
+    return if attr.blank? || attr == "NA"
+
+    content_tag(:p, "#{name}: #{attr}")
+  end
 end
--- spec/helpers/trends_helper_spec.rb
+++ spec/helpers/trends_helper_spec.rb
@@ -3,13 +3,24 @@ require "rails_helper"
 # Specs in this file have access to a helper object that includes
 # the TrendsHelper. For example:
 #
-# describe TrendsHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       expect(helper.concat_strings("this","that")).to eq("this that")
-#     end
-#   end
-# end
-RSpec.describe TrendsHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe TrendsHelper, type: :helper do
+  describe "paragraph_if_present" do
+    context "with value" do
+      it "returns the value in a <p> tag" do
+        expect(helper.paragraph_if_present("title", "hello")).to eq("<p>title: hello</p>")
+      end
+    end
+
+    context "with no value" do
+      it do
+        expect(helper.paragraph_if_present("title", nil)).to be nil
+      end
+    end
+
+    context "with no value == 'NA'" do
+      it do
+        expect(helper.paragraph_if_present("title", "NA")).to be nil
+      end
+    end
+  end
 end

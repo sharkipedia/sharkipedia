1476bfa0163b0045639fef3865f78699a7a3d90f
Maximilian Haack <max@haack.xyz>
2020-07-24 18:22:23 +0200

    Add missing has_many associations to Standard

--- spec/models/standard_spec.rb
+++ spec/models/standard_spec.rb
@@ -1,5 +1,8 @@
 require "rails_helper"
 
 RSpec.describe Standard, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "associations" do
+    it { should have_many(:measurements) }
+    it { should have_many(:trends) }
+  end
 end

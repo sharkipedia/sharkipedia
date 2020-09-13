72f217ffdb249be27449d6a79e40b0bbb618d5e6
Maximilian Haack <max@haack.xyz>
2020-09-13 22:05:26 +0200

    Add serializers

---
+++ app/serializers/measurement_serializer.rb
@@ -0,0 +1,47 @@
+class MeasurementSerializer < BaseSerializer
+  attributes :value
+  attribute :sex_type do |object|
+    object.sex_type&.name
+  end
+  attribute :trait_class do |object|
+    object.trait_class&.name
+  end
+  attribute :trait do |object|
+    object.trait&.name
+  end
+  attribute :standard do |object|
+    object.standard&.name
+  end
+  attribute :measurement_method do |object|
+    object.measurement_method&.name
+  end
+  attribute :measurement_model do |object|
+    object.measurement_model&.name
+  end
+  attribute :value_type do |object|
+    object.value_type&.name
+  end
+  attribute :precision
+  attribute :precision_type do |object|
+    object.precision_type&.name
+  end
+  attribute :precision_upper
+  attribute :sample_size
+  attribute :dubious
+  attribute :validated
+  attribute :notes
+  attribute :validation_type do |object|
+    object.validation_type&.name
+  end
+  attribute :location do |object|
+    object.location&.name
+  end
+  attribute :ocean do |object|
+    object.location&.name
+  end
+  attribute :longhurst_province do |object|
+    object.longhurst_province&.name
+  end
+
+  belongs_to :observation
+end
---
+++ app/serializers/species_serializer.rb
@@ -0,0 +1,16 @@
+class SpeciesSerializer < BaseSerializer
+  attribute :binomial_name, &:name
+
+  attributes :edge_scientific_name, :iucn_code, :authorship
+
+  # :family, :order, :subclass, :superorder
+
+  # belongs_to :species_superorder
+  # belongs_to :species_data_type
+  # belongs_to :species_subclass
+  # belongs_to :species_order
+  # belongs_to :species_family
+
+  has_many :observations
+  has_many :trends
+end

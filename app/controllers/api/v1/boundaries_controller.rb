a6d3bd7efca040820d12c9721799be13cd381b4f
Maximilian Haack <max@haack.xyz>
2020-11-29 15:11:51 +0100

    Add EEZs to BoundariesController

--- app/controllers/api/v1/boundaries_controller.rb
+++ app/controllers/api/v1/boundaries_controller.rb
@@ -1,7 +1,10 @@
 module API::V1
   class BoundariesController < APIController
     Boundary = Struct.new(:id, :name) {}
-    BOUNDARIES = [Boundary.new(1, "Ocean")]
+    BOUNDARIES = [
+      Boundary.new(1, "Ocean"),
+      Boundary.new(2, "Eez")
+    ]
 
     def index
       render jsonapi: BOUNDARIES
@@ -11,10 +14,23 @@ module API::V1
       case params[:name]
       when /Oceans?/i
         render jsonapi: Ocean.all
+      when /eezs?/i
+        jsonapi_paginate(Eez.select_without_geom) do |paginated|
+          render jsonapi: paginated
+        end
       end
     end
 
     private
+
+    def jsonapi_meta(resources)
+      pagination = jsonapi_pagination_meta(resources)
+
+      {
+        total: (resources.count if resources.respond_to?(:count)),
+        pagination: (pagination if pagination.present?)
+      }.compact
+    end
 
     def jsonapi_serializer_class(resource, is_collection)
       JSONAPI::Rails.serializer_class(resource, is_collection)
--- spec/controllers/api/v1/boundaries_controller_spec.rb
+++ spec/controllers/api/v1/boundaries_controller_spec.rb
@@ -18,15 +18,25 @@ RSpec.describe API::V1::BoundariesController, type: :controller do
 
   describe "GET #show" do
     let!(:atlantic) { create :ocean, name: "Atlantic" }
+    let(:asm) { Eez.find_by iso_ter1: "ASM" }
 
     it "returns http success" do
       get :show, params: {name: "oceans"}
       expect(response).to have_http_status(:success)
     end
 
-    it "returns the details of the selected boundary type" do
-      get :show, params: {name: "oceans"}
-      expect(response.body).to be_include(atlantic.name)
+    context "when oceans" do
+      it "returns the details all available oceans" do
+        get :show, params: {name: "oceans"}
+        expect(response.body).to be_include(atlantic.name)
+      end
+    end
+
+    context "when EEZ" do
+      it "returns the details all available EEZs" do
+        get :show, params: {name: "eez"}
+        expect(response.body).to be_include(asm.geoname)
+      end
     end
   end
 end

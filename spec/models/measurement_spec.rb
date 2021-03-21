83ac10319f6ff0aab9ce819f0f3e4a0eddb0556a
Maximilian Haack <max@haack.xyz>
2021-03-21 17:14:46 -0400

    Add association specs for Measurement

--- spec/models/measurement_spec.rb
+++ spec/models/measurement_spec.rb
@@ -1,5 +1,18 @@
 require "rails_helper"
 
 RSpec.describe Measurement, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "associations" do
+    it { should belong_to(:observation) }
+    it { should belong_to(:longhurst_province).optional }
+    it { should belong_to(:sex_type) }
+    it { should belong_to(:trait) }
+    it { should have_one(:trait_class) }
+    it { should belong_to(:standard).optional }
+    it { should belong_to(:measurement_method).optional }
+    it { should belong_to(:measurement_model).optional }
+    it { should belong_to(:value_type).optional }
+    it { should belong_to(:validation_type).optional }
+    it { should belong_to(:precision_type).optional }
+    it { should belong_to(:location) }
+  end
 end

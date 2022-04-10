cc80526425a531e45bf8aa3376966780efc5b145
Ann Cascarano <4411121+redrambles@users.noreply.github.com>
2022-04-10 15:52:44 -0400

    Remove dummy test file

--- app/javascript/Dinosaur.js
+++
@@ -1,5 +0,0 @@
-export default class Dinosaur {
-	get isExtinct() {
-		return true;
-	}
-}
--- spec/javascript/dinosaur.spec.js
+++
@@ -1,4 +0,0 @@
-import Dinosaur from "Dinosaur";
-test("Dinosaurs are extinct", () => {
-	expect(new Dinosaur().isExtinct).toBeTruthy();
-});

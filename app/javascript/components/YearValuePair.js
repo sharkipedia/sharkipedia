ca9172cdf40d6585f82cc84b9ece74e90121c327
Ann Cascarano <4411121+redrambles@users.noreply.github.com>
2022-05-01 22:02:52 -0400

    Format with Prettier

--- app/javascript/components/YearValuePair.js
+++ app/javascript/components/YearValuePair.js
@@ -1,34 +1,40 @@
-import React from 'react'
+import React from "react";
 
 const YearValuePair = (props) => {
+	const handleChange = (e) => {
+		const year = document.getElementById(e.target.id.replace("value", "year")).value;
+		props.onYearChange(year, e.target.value);
+	};
 
-    const handleChange = (e) => {
-       const year = document.getElementById(e.target.id.replace("value", "year")).value;
-       props.onYearChange(year, e.target.value);
-     }
+	const yearId = "trend_observations_year_" + props.year;
+	const valueId = "trend_observations_value_" + props.year;
 
-     const yearId = "trend_observations_year_" + props.year;
-     const valueId = "trend_observations_value_" + props.year;
+	return (
+		<div className='field is-horizontal'>
+			<div className='field-body'>
+				<div className='field'>
+					<div className='control'>
+						<input className='input' readOnly={true} tabIndex='-1' aria-label='year' type='text' value={props.year} id={yearId} />
+						<br />
+					</div>
+				</div>
 
-     return (
-       <div className="field is-horizontal">
-         <div className="field-body">
-           <div className="field">
-             <div className="control">
-               <input className="input" readOnly={true} tabIndex="-1" aria-label="year" type="text" value={props.year} id={yearId} /><br />
-             </div>
-           </div>
+				<div className='field'>
+					<div className='control'>
+						<input
+							className='input trend-value'
+							step='any'
+							aria-label='value'
+							type='text'
+							value={props.value}
+							id={valueId}
+							onChange={handleChange}
+						/>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
 
-           <div className="field">
-             <div className="control">
-               <input className="input trend-value" step="any" aria-label="value" type="text" value={props.value} id={valueId} onChange={handleChange} />
-             </div>
-           </div>
-         </div>
-       </div>
-     );
- }
-
-
-export default YearValuePair
-
+export default YearValuePair;
--- spec/javascript/YearValuePair.spec.js
+++ spec/javascript/YearValuePair.spec.js
@@ -5,24 +5,24 @@ import YearValuePair from "../../app/javascript/components/YearValuePair";
 
 test("Should see year input", () => {
 	render(<YearValuePair year={"1999"} value={""} />);
-    const yearInput = screen.getByLabelText("year");
+	const yearInput = screen.getByLabelText("year");
 	expect(yearInput).toBeInTheDocument();
 });
 
 test("Should see value input", () => {
 	render(<YearValuePair year={"1999"} value={""} />);
-    const valueInput = screen.getByLabelText("value");
+	const valueInput = screen.getByLabelText("value");
 	expect(valueInput).toBeInTheDocument();
 });
 
 test("Year input should display year passed as year prop", () => {
-    render(<YearValuePair year={"1999"} value={""} />);
-    const yearInput = screen.getByLabelText("year");
+	render(<YearValuePair year={"1999"} value={""} />);
+	const yearInput = screen.getByLabelText("year");
 	expect(yearInput.value).toBe("1999");
-})
+});
 
 test("Value input should display value", () => {
 	render(<YearValuePair year={"1999"} value={"22"} />);
-    const valueInput = screen.getByLabelText("value");
+	const valueInput = screen.getByLabelText("value");
 	expect(valueInput.value).toBe("22");
 });

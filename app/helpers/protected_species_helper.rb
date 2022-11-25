97f3194a1fd402e8d3b4c84a3284deb770952d0b
Ann Cascarano <4411121+redrambles@users.noreply.github.com>
2022-11-25 12:24:08 -0500

    Format protection status in view

--- app/helpers/protected_species_helper.rb
+++ app/helpers/protected_species_helper.rb
@@ -1,2 +1,12 @@
 module ProtectedSpeciesHelper
+  def format_protected_status(status)
+    case status
+    when "none"
+      nil
+    when "appendix_1"
+      "Appendix I"
+    when "appendix_2"
+      "Appendix II"
+    end
+  end
 end

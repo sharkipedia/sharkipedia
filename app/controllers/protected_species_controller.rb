1725ff241b7676cca932a514923bb2105dff4c51
Ann Cascarano <4411121+redrambles@users.noreply.github.com>
2022-11-29 09:09:51 -0500

    Allow unrestricted access to Protected Species View

--- app/controllers/protected_species_controller.rb
+++ app/controllers/protected_species_controller.rb
@@ -1,4 +1,4 @@
-class ProtectedSpeciesController < ApplicationController
+class ProtectedSpeciesController < PreAuthController
   def index
     @species = policy_scope(Species).where.not(cms_status: 0).or(policy_scope(Species).where.not(cites_status: 0))
   end

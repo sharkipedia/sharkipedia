2bb5fab2738d7f8e75680f4c702be67eba69014a
Ann Cascarano <4411121+redrambles@users.noreply.github.com>
2022-12-03 11:23:00 -0500

    Fix spec for protected_species request

--- spec/requests/protected_species_spec.rb
+++ spec/requests/protected_species_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 RSpec.describe "ProtectedSpecies", type: :request do
   describe "GET /index" do
     it "returns http success" do
-      get "/protected_species/index"
+      get "/protected_species"
       expect(response).to have_http_status(:success)
     end
   end

f5f992350e8fcd1dcd28319098c7939af004ffd8
Maximilian Haack <max@haack.xyz>
2022-12-10 17:41:11 -0500

    Remove dead puma configuration params

--- config/puma.rb
+++ config/puma.rb
@@ -8,10 +8,6 @@ max_threads_count = ENV.fetch("RAILS_MAX_THREADS", 5)
 min_threads_count = ENV.fetch("RAILS_MIN_THREADS", max_threads_count)
 threads min_threads_count, max_threads_count
 
-wait_for_less_busy_worker
-
-nakayoshi_fork
-
 # Specifies the `worker_timeout` threshold that Puma will use to wait before
 # terminating a worker in development environments.
 #

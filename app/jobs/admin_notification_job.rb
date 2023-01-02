fcca0ad3eca794b8c21c5c6ba9dab2b5ef51c470
Ann Cascarano <4411121+redrambles@users.noreply.github.com>
2023-01-02 15:11:10 -0500

    Extract guard

--- app/jobs/admin_notification_job.rb
+++ app/jobs/admin_notification_job.rb
@@ -2,11 +2,15 @@ class AdminNotificationJob < ApplicationJob
   queue_as :default
 
   def perform
-    return unless Rails.env.production?
-    return unless Import.pending_review.any?
+    return unless should_run?
 
-    admins = User.admins
-    emails = admins.map(&:email)
+    emails = User.admin_emails
     ImportMailer.with(emails: emails).weekly_import_summary_email.deliver_later
+  end
+
+  private
+
+  def should_run?
+    Rails.env.production? && Import.pending_review.any?
   end
 end

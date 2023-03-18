a1043e17949264d19c7dd151cc8d45271e885875
Maximilian Haack <max@haack.xyz>
2023-03-18 18:06:04 -0400

    Add treaties_species backfill

---
+++ docs/treaties_species_backfill.rb
@@ -0,0 +1,175 @@
+csv = <<~CSV
+Acroteriobatus annulatus,,2022,,,
+Acroteriobatus blochii,,2022,,,
+Acroteriobatus leucospilus,,2022,,,
+Acroteriobatus ocellatus,,2022,,,
+Acroteriobatus omanensis,,2022,,,
+Acroteriobatus salalah,,2022,,,
+Acroteriobatus variegatus,,2022,,,
+Acroteriobatus zanzibarensis,,2022,,,
+Alopias pelagicus,,2017,,2014,
+Alopias superciliosus,,2017,,2014,
+Alopias vulpinus,,2017,,2014,
+Anoxypristis cuspidata,2007,,2014,,
+Carcharhinus acronotus,,2022,,,
+Carcharhinus albimarginatus,,2022,,,
+Carcharhinus altimus,,2022,,,
+Carcharhinus amblyrhynchoides,,2022,,,
+Carcharhinus amblyrhynchos,,2022,,,
+Carcharhinus amboinensis,,2022,,,
+Carcharhinus borneensis,,2022,,,
+Carcharhinus brachyurus,,2022,,,
+Carcharhinus brevipinna,,2022,,,
+Carcharhinus cautus,,2022,,,
+Carcharhinus cerdale,,2022,,,
+Carcharhinus coatesi,,2022,,,
+Carcharhinus dussumieri,,2022,,,
+Carcharhinus falciformis,,2017,,2014,
+Carcharhinus fitzroyensis,,2022,,,
+Carcharhinus galapagensis,,2022,,,
+Carcharhinus hemiodon,,2022,,,
+Carcharhinus humani,,2022,,,
+Carcharhinus isodon,,2022,,,
+Carcharhinus leiodon,,2022,,,
+Carcharhinus leucas,,2022,,,
+Carcharhinus limbatus,,2022,,,
+Carcharhinus longimanus,,2013,2020,,
+Carcharhinus macloti,,2022,,,
+Carcharhinus melanopterus,,2022,,,
+Carcharhinus obscurus,,2022,,,
+Carcharhinus perezi,,2022,,,
+Carcharhinus plumbeus,,2022,,,
+Carcharhinus porosus,,2022,,,
+Carcharhinus sealei,,2022,,,
+Carcharhinus signatus,,2022,,,
+Carcharhinus sorrah,,2022,,,
+Carcharhinus tilstoni,,2022,,,
+Carcharhinus tjutjot,,2022,,,
+Carcharodon carcharias,,2005,2002,,
+Cetorhinus maximus,,2003,2005,,
+Eusphyra blochii,,2022,,,
+Galeocerdo cuvier,,2022,,,
+Galeorhinus galeus,,,,2020,
+Glaucostegus cemiculus,,2019,,,
+Glaucostegus granulatus,,2019,,,
+Glaucostegus halavi,,2019,,,
+Glaucostegus obtusus,,2019,,,
+Glaucostegus thouin,,2019,,,
+Glaucostegus typus,,2019,,,
+Glyphis gangeticus,,2022,,,
+Glyphis garricki,,2022,,,
+Glyphis glyphis,,2022,,,
+Isogomphodon oxyrhynchus,,2022,,,
+Isurus oxyrinchus,,2019,,2008,
+Isurus paucus,,2019,,2008,
+Lamiopsis temminckii,,2022,,,
+Lamiopsis tephrodes,,2022,,,
+Lamna nasus,,2013,,2008,
+Loxodon macrorhinus,,2022,,,
+Mobula alfredi,,2013,2014,,
+Mobula birostris,,2013,2011,,
+Mobula hypostoma,,2017,2014,,
+Mobula kuhlii,,2017,2014,,
+Mobula mobular,,2017,2014,,
+Mobula munkiana,,2017,2014,,
+Mobula tarapacana,,2017,2014,,
+Mobula thurstoni,,2017,2014,,
+Nasolamia velox,,2022,,,
+Negaprion acutidens,,2022,,,
+Negaprion brevirostris,,2022,,,
+Potamotrygon henlei,,2022,,,
+Potamotrygon leopoldi,,2022,,,
+Potamotrygon signata,,2022,,,
+Prionace glauca,,2022,,2017,
+Pristis clavata,2007,,2014,,
+Pristis pectinata,2007,,2014,,
+Pristis pristis,2007,,2014,,
+Pristis zijsron,2007,,2014,,
+Pseudobatos glaucostigmus,,2022,,,
+Pseudobatos horkelii,,2022,,,
+Pseudobatos lentiginosus,,2022,,,
+Pseudobatos leucorhynchus,,2022,,,
+Pseudobatos percellens,,2022,,,
+Pseudobatos planiceps,,2022,,,
+Pseudobatos prahli,,2022,,,
+Pseudobatos productus,,2022,,,
+Rhina ancylostoma,,2019,,,
+Rhincodon typus,,2003,2017,1999,
+Rhinobatos albomaculatus,,2022,,,
+Rhinobatos annandalei,,2022,,,
+Rhinobatos borneensis,,2022,,,
+Rhinobatos holcorhynchus,,2022,,,
+Rhinobatos hynnicephalus,,2022,,,
+Rhinobatos irvinei,,2022,,,
+Rhinobatos jimbaranensis,,2022,,,
+Rhinobatos lionotus,,2022,,,
+Rhinobatos manai,,2022,,,
+Rhinobatos nudidorsalis,,2022,,,
+Rhinobatos penggali,,2022,,,
+Rhinobatos punctifer,,2022,,,
+Rhinobatos rhinobatos,,2022,,2017,
+Rhinobatos sainsburyi,,2022,,,
+Rhinobatos schlegelii,,2022,,,
+Rhinobatos whitei,,2022,,,
+Rhizoprionodon acutus,,2022,,,
+Rhizoprionodon lalandii,,2022,,,
+Rhizoprionodon longurio,,2022,,,
+Rhizoprionodon oligolinx,,2022,,,
+Rhizoprionodon porosus,,2022,,,
+Rhizoprionodon taylori,,2022,,,
+Rhizoprionodon terraenovae,,2022,,,
+Rhynchobatus australiae,,2019,,2017,
+Rhynchobatus cooki,,2019,,,
+Rhynchobatus djiddensis,,2019,,2017,
+Rhynchobatus immaculatus,,2019,,,
+Rhynchobatus laevis,,2019,,2017,
+Rhynchobatus luebberti,,2019,,,
+Rhynchobatus palpebratus,,2019,,,
+Rhynchobatus springeri,,2019,,,
+Rhynchorhina mauritaniensis,,2019,,,
+Scoliodon laticaudus,,2022,,,
+Scoliodon macrorhynchos,,2022,,,
+Sphyrna corona,,2022,,,
+Sphyrna gilberti,,2022,,,
+Sphyrna lewini,,2013,,2014,
+Sphyrna media,,2022,,,
+Sphyrna mokarran,,2013,,2014,
+Sphyrna tiburo,,2022,,,
+Sphyrna tudes,,2022,,,
+Sphyrna zygaena,,2013,,2020,
+Squalus acanthias,,,,2008,
+Squatina squatina,,,,2017,
+Triaenodon obesus,,2022,,,
+CSV
+
+s = csv.split("\n").map { |schedule| schedule.split(",") }
+
+s.map do |species_name, cites_1, cites_2, cms_1, cms_2|
+  species = Species.find_by(name: species_name)
+  cites_1 = nil if cites_1.blank?
+  cites_2 = nil if cites_2.blank?
+
+  cms_1 = nil if cms_1.blank?
+  cms_2 = nil if cms_2.blank?
+
+  if cites_1
+    species.cites_status = :appendix_1
+  elsif cites_2
+    species.cites_status = :appendix_2
+  end
+  species.cites_status_year = (cites_1 || cites_2)
+
+    if cms_1
+    species.cms_status = :appendix_1
+    elsif cms_2
+    species.cms_status = :appendix_2
+    end
+    species.cms_status_year = (cms_1 || cms_2)
+
+  species.save!
+  species
+rescue => e
+  require "byebug"; byebug
+  puts e.message
+end
+

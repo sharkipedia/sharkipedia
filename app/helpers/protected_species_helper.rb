d5292d48e4a4323fccc51d31fc51e96e94a8017a
Ann <4411121+redrambles@users.noreply.github.com>
2023-03-18 16:44:10 -0400

    Show schedule year

--- app/helpers/protected_species_helper.rb
+++ app/helpers/protected_species_helper.rb
@@ -1,12 +1,13 @@
 module ProtectedSpeciesHelper
-  def format_protected_status(status)
+  def format_protected_status(status, year)
     case status
     when "none"
       nil
     when "appendix_1"
-      "Appendix I"
+      # Temp fix - right now now all of our Protected Species have populated year colums
+      year ? "Appendix I (#{year})" : "Appendix I"
     when "appendix_2"
-      "Appendix II"
+      year ? "Appendix II (#{year})" : "Appendix II"
     end
   end
 

f1817702e18efe1f417ecfd4b8ddd8e897e9617b
Ann Cascarano <4411121+redrambles@users.noreply.github.com>
2023-03-25 11:58:35 -0400

    Add year to international_species_status helper

--- app/helpers/species_helper.rb
+++ app/helpers/species_helper.rb
@@ -1,22 +1,12 @@
 module SpeciesHelper
   include Pagy::Frontend
-
-  def format_status(status)
-    case status
-    when "appendix_1"
-      "Appendix I"
-    when "appendix_2"
-      "Appendix II"
-    when "appendix_3"
-      "Appendix III"
-    end
-  end
+  include ProtectedSpeciesHelper
 
   def international_species_status(species)
-    if species.cites_status != "none" || species.cms_status != "none"
+    if species.protected_species?
       content_tag(:h5, "Species Regulated by International Treaties", class: "title is-5 mt-3 mb-2") +
-        ((species.cites_status != "none") ? content_tag(:p, "CITES: #{format_status(species.cites_status)}") : "") +
-        ((species.cms_status != "none") ? content_tag(:p, "CMS: #{format_status(species.cms_status)}") : "")
+        (species.cites_status_none? ? "" : content_tag(:p, "CITES: #{format_protected_status(species.cites_status, species.cites_status_year)} ")) +
+        (species.cms_status_none? ? "" : content_tag(:p, "CMS: #{format_protected_status(species.cms_status, species.cms_status_year)}"))
     end
   end
 end

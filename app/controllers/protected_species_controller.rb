bb69f430277c01664f712835f34f7b285322aa54
Ann Cascarano <4411121+redrambles@users.noreply.github.com>
2023-04-15 13:34:01 -0400

    Filter Protected Species by Order

--- app/controllers/protected_species_controller.rb
+++ app/controllers/protected_species_controller.rb
@@ -1,5 +1,11 @@
 class ProtectedSpeciesController < PreAuthController
   def index
     @species = policy_scope(Species).where.not(cms_status: 0).or(policy_scope(Species).where.not(cites_status: 0)).includes(:species_order, :species_family).order("species_order.name, species_families.name, species.name")
+
+    @species_orders = @species.map { |s| s.species_order.name }.uniq
+
+    if params.dig :filter, :order
+      @species = @species.where(species_order: {name: params[:filter][:order]})
+    end
   end
 end

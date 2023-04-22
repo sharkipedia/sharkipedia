1da23a7ad68608e8438b1664aa5a6680a567bc32
Ann Cascarano <4411121+redrambles@users.noreply.github.com>
2023-04-22 12:47:51 -0400

    Drop helper methods

--- app/controllers/protected_species_controller.rb
+++ app/controllers/protected_species_controller.rb
@@ -4,25 +4,14 @@ class ProtectedSpeciesController < PreAuthController
     @species_orders = @species.map { |s| s.species_order.name }.uniq
     @species_families = @species.map { |s| s.species_family.name }.uniq.sort
 
-    if filtered_by_order?
-      @species = @species.where(species_order: {name: params[:filter][:order]})
+    if (@current_order = params.dig(:filter, :order))
+      @species = @species.where(species_order: {name: @current_order})
       @species_families = @species.map { |s| s.species_family.name }.uniq.sort
-      @current_order = params[:filter][:order]
     end
 
-    if filtered_by_family?
-      @species = @species.where(species_families: {name: params[:filter][:family]})
+    if (@current_family = params.dig(:filter, :family))
+      @species = @species.where(species_families: {name: @current_family})
       @species_orders = @species.map { |s| s.species_order.name }.uniq
     end
-  end
-
-  private
-
-  def filtered_by_order?
-    params.dig :filter, :order
-  end
-
-  def filtered_by_family?
-    params.dig :filter, :family
   end
 end

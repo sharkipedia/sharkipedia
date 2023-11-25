51a6b2758b51686632b76575f4d31763c01a2ed8
Ann Cascarano <4411121+redrambles@users.noreply.github.com>
2023-11-25 14:07:03 -0500

    Fix linting

--- spec/models/trait_class_spec.rb
+++ spec/models/trait_class_spec.rb
@@ -1,14 +1,14 @@
 require "rails_helper"
 
 RSpec.describe TraitClass, type: :model do
-  describe 'validations' do
-    subject { build(:trait_class)}
+  describe "validations" do
+    subject { build(:trait_class) }
 
-    it {is_expected.to validate_presence_of(:name)}
-    it {is_expected.to validate_uniqueness_of(:name)}
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_uniqueness_of(:name) }
   end
 
-  describe 'associations' do
+  describe "associations" do
     it { should have_many(:traits) }
     it { should have_many(:standards) }
     it { should have_many(:measurement_methods) }
